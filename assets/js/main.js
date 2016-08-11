c1e2ee397740369ad3ebcfe8d12368c64eefb412
Angular417 <borislav.dimitrov417@gmail.com>
2016-08-12 00:22:34 +0300

    building borrow pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,20 @@ $(document).ready(function() {
     interval: 2000
   });
 
-  //go to the back card page
+  // go to the back card page
   $(document).on('click', '#loan_modal .request-loan', function() {
   	window.location ="card.html";
+  });
+
+  // set form width as windows one in back card page
+  function setCarFormHeight() {
+  	$('.main-loan-area').height($(document).height() - $('.header').height());
+  }
+  
+  setCarFormHeight();
+
+  $(window).resize(function() {
+    setCarFormHeight();
   });
 
 });
