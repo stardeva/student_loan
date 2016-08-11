150a03a309d4953df08b690d7446a88e4c1e2272
Angular417 <borislav.dimitrov417@gmail.com>
2016-08-11 05:08:40 +0300

    loan project

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $('.carousel').carousel({
     interval: 2000
   });
+
+  //go to the back card page
+  $(document).on('click', '#loan_modal .request-loan', function() {
+  	window.location ="card.html";
+  });
+
 });
