640a0cf322626b6262f2afab3072cecbae7fe742
stardeva <stardeva111@gmail.com>
2016-09-23 05:36:51 +0200

    fix datepicker

--- credits/credit_base.php
+++ credits/credit_base.php
@@ -355,6 +355,7 @@ if(checkUserLogin()) {
 
     <script type="text/javascript" src="../assets/js/jquery-2.1.4.min.js"></script>
     <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../assets/js/bootstrap-datepicker.min.js"></script>
     <script type="text/javascript" src="../assets/js/swiper.min.js"></script>
     <script type="text/javascript" src="../assets/js/jquery.popupoverlay.js"></script>
     <script type="text/javascript" src="../assets/js/select2.min.js"></script>
